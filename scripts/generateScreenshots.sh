17647447019842cd6d6b9542ba5f6e6b71fc95e4
jscasca <jscasca@gmail.com>
2019-01-23 09:38:58 +1100

    Screencapturing tools for dev

--- scripts/generateScreenshots.sh
+++ scripts/generateScreenshots.sh
@@ -2,15 +2,34 @@
 # wkhtmltoimage --height <height> --width <width> --filename="<name>" <url>
 # <url> = http://localhost/prlg_styles/templates/<name>
 
-#rm output/*
+# View ports [360x640, 768x1204, 600x960]
+# Iphone [320x568 - 414x896]
+# iPad [768x1024 - 1024x1366]
+# Android [360x740 - 480x853]
+# Android tablet [600x960 - 800x1280 - 1280x850]
+## Remove old stuff
+rm -rf output/*
+mkdir output/phone
+mkdir output/tablet
+# mkdir output/web
 concatenate=""
+## Iterate
 for f in ./templates/*.html; do
   # do some stuff with $f
   name_html="$(cut -d'/' -f3 <<<"$f")"
   name="$(cut -d'.' -f1 <<<"$name_html")"
-  #wkhtmltoimage --height 1200 --width 450 "http://localhost/prlg_styles/templates/$name.html" "output/$name.jpg"
+  wkhtmltoimage --height 800 --width 414 "http://localhost/prlg_styles/templates/$name.html" "output/phone/$name.jpg"
+  wkhtmltoimage --height 800 --width 760 "http://localhost/prlg_styles/templates/$name.html" "output/tablet/$name.jpg"
+  wkhtmltoimage --height 800 --width 1200 "http://localhost/prlg_styles/templates/$name.html" "output/web/$name.jpg"
   echo $name
-  concatenate="$concatenate ./output/$name.jpg"
+  concatenate="$concatenate ./$name.jpg"
 done
 #pwd
-`convert $concatenate ./output/merged.pdf`
+cd output/phone
+`convert -density 300 -quality 100 $concatenate ../phone.pdf`
+cd ../tablet
+`convert -density 300 -quality 100 $concatenate ../tablet.pdf`
+cd ../web
+`convert -density 300 -quality 100 $concatenate ../web.pdf`
+cd ../
+convert -density 300 -quality 100 phone.pdf tablet.pdf web.pdf merge.pdf
