6714659918b8d0f0d1d11af4db45011336630683
Scarlet <scarlet.brekke@gmail.com>
2019-02-27 13:22:30 +1000

    Updates
    Changed background
    Added dropdown menu to profile button in nav bar

--- scripts/group.js
+++ scripts/group.js
@@ -14,4 +14,53 @@ $(document).ready(function () {
 	  console.log("Hide");
     $(this).siblings('.panel-heading').removeClass('active');
   });
+	
+	
+  //Use these two variables to save existing text to restore if user cancels
+  var savedTitle ='';
+  var savedDescription = '';
+	
+  var editButton = $('#edit-club-description-button');
+	
+  $(editButton).click(function() {
+	  
+	  var clubTitle = $('#club-title');
+	  var clubDesc = $('#club-description');
+	  var actionButtons = $('#edit-description-action-buttons');
+	  
+	  $(clubTitle).prop('disabled', false);
+	  $(clubDesc).prop('disabled', false);
+	  $(actionButtons).prop('hidden', false);
+	  $(editButton).prop('hidden', true);
+	  
+	  savedTitle=$(clubTitle).val();
+	  savedDescription=$(clubDesc).val();
+  });	
+  
+	$('#cancel-club-description-button').click(function() {
+		var clubTitle = $('#club-title');
+	  	var clubDesc = $('#club-description');
+	  	var actionButtons = $('#edit-description-action-buttons');
+		
+		$(clubTitle).prop('disabled', true);
+	  	$(clubDesc).prop('disabled', true);
+		$(actionButtons).prop('hidden', true);
+		$(editButton).prop('hidden', false);
+		
+		$(clubTitle).val(savedTitle);
+		$(clubDesc).val(savedDescription);
+	});
+	
+	$('#save-club-description-button').click(function() {
+		//This is where the backend needs to be called
+		
+		var clubTitle = $('#club-title');
+	  	var clubDesc = $('#club-description');
+	  	var actionButtons = $('#edit-description-action-buttons');
+		
+		$(clubTitle).prop('disabled', true);
+	  	$(clubDesc).prop('disabled', true);
+		$(actionButtons).prop('hidden', true);
+		$(editButton).prop('hidden', false);
+	});
 });
