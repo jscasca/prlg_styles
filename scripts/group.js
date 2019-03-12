a6e80a177153e981b106016b2952660ea43c296c
Scarlet <scarlet.brekke@gmail.com>
2019-03-12 11:13:11 +1000

    Admin page coming along
    Contains:
    - "Change image" btn with Java
    - "Edit" club description with Java
    - A right hand panel designed for admin only with "more" btns (that I cannot make listen to CSS, please help)

--- scripts/group.js
+++ scripts/group.js
@@ -1,5 +1,6 @@
 // JavaScript Document
 
+
 function readUrl(input) {
 	"use strict";
 	
@@ -101,3 +102,34 @@ $(document).ready(function () {
 		$('.profile-image-placeholder').attr('src',  savedSrc);
 	});
 });
+
+var el = document.querySelector('.more');
+var btn = el.querySelector('.more-btn');
+var menu = el.querySelector('.more-menu');
+var visible = false;
+
+function showMenu(e) {
+	"use strict";
+    e.preventDefault();
+    if (!visible) {
+        visible = true;
+        el.classList.add('show-more-menu');
+        menu.setAttribute('aria-hidden', false);
+        document.addEventListener('mousedown', hideMenu, false);
+    }
+}
+
+function hideMenu(e) {
+	"use strict";
+    if (btn.contains(e.target)) {
+        return;
+    }
+    if (visible) {
+        visible = false;
+        el.classList.remove('show-more-menu');
+        menu.setAttribute('aria-hidden', true);
+        document.removeEventListener('mousedown', hideMenu);
+    }
+}
+
+btn.addEventListener('click', showMenu, false);
